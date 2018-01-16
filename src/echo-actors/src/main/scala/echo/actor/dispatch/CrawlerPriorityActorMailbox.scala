a6b0340a9cff2e1ddee618b883d7a99a43ff27f9
Maximilian Irro <max@irro.at>
2018-01-16 11:33:34 +0100

    [actor] added a custom actor dispatcher for crawler

---
+++ src/echo-actors/src/main/scala/echo/actor/dispatch/CrawlerPriorityActorMailbox.scala
@@ -0,0 +1,20 @@
+package echo.actor.dispatch
+
+import akka.actor.ActorSystem
+import akka.dispatch.{PriorityGenerator, UnboundedPriorityMailbox}
+import com.typesafe.config.Config
+import echo.actor.protocol.ActorMessages._
+
+/**
+  * @author Maximilian Irro
+  */
+class CrawlerPriorityActorMailbox(settings: ActorSystem.Settings, config: Config) extends UnboundedPriorityMailbox(
+    // Create a new PriorityGenerator, lower prio means more important
+    PriorityGenerator {
+        case ActorRefDirectoryStoreActor(_) => 0
+        case FetchNewFeed(_,_)  => 1
+        case CrawlFyyd(_)       => 1
+        case FetchUpdateFeed(_,_,_) => 2
+        case FetchWebsite(_,_)      => 2
+        case _ => 3
+    })
