4d009abcd5c7ca622ea69bea9def2711029c01a9
Maximilian Irro <max@irro.at>
2018-01-16 13:31:20 +0100

    [actor] serialize itunesDuration for episodes

--- src/echo-actors/src/main/scala/echo/actor/gateway/json/EpisodeJsonProtocol.scala
+++ src/echo-actors/src/main/scala/echo/actor/gateway/json/EpisodeJsonProtocol.scala
@@ -11,16 +11,17 @@ import spray.json.{DefaultJsonProtocol, DeserializationException, JsNull, JsObje
 object EpisodeJsonProtocol extends DefaultJsonProtocol {
     implicit object EpisodeJsonFormat extends RootJsonFormat[EpisodeDTO] {
         def write(e: EpisodeDTO) = JsObject(
-            "echoId"      -> Option(e.getEchoId).map(value => JsString(value)).getOrElse(JsNull),
-            "title"       -> Option(e.getTitle).map(value => JsString(value)).getOrElse(JsNull),
-            "link"        -> Option(e.getLink).map(value => JsString(value)).getOrElse(JsNull),
-            "pubDate"     -> Option(e.getPubDate).map(value => JsString(value.toString)).getOrElse(JsNull),
-            "description" -> Option(e.getDescription).map(value => JsString(value)).getOrElse(JsNull),
-            "itunesImage" -> Option(e.getItunesImage).map(value => JsString(value)).getOrElse(JsNull)
+            "echoId"         -> Option(e.getEchoId).map(value => JsString(value)).getOrElse(JsNull),
+            "title"          -> Option(e.getTitle).map(value => JsString(value)).getOrElse(JsNull),
+            "link"           -> Option(e.getLink).map(value => JsString(value)).getOrElse(JsNull),
+            "pubDate"        -> Option(e.getPubDate).map(value => JsString(value.toString)).getOrElse(JsNull),
+            "description"    -> Option(e.getDescription).map(value => JsString(value)).getOrElse(JsNull),
+            "itunesImage"    -> Option(e.getItunesImage).map(value => JsString(value)).getOrElse(JsNull),
+            "itunesDuration" -> Option(e.getItunesDuration).map(value => JsString(value)).getOrElse(JsNull)
         )
         def read(value: JsValue) = {
-            value.asJsObject.getFields("echoId", "title", "link", "pubDate", "description", "itunesImage") match {
-                case Seq(JsString(echoId), JsString(title), JsString(link),  JsString(pubDate), JsString(description), JsString(itunesImage)) => {
+            value.asJsObject.getFields("echoId", "title", "link", "pubDate", "description", "itunesImage", "itunesDuration") match {
+                case Seq(JsString(echoId), JsString(title), JsString(link),  JsString(pubDate), JsString(description), JsString(itunesImage), JsString(itunesDuration)) => {
                     val episode = new EpisodeDTO()
                     episode.setEchoId(echoId)
                     episode.setTitle(title)
@@ -28,6 +29,7 @@ object EpisodeJsonProtocol extends DefaultJsonProtocol {
                     episode.setPubDate(LocalDateTime.parse(pubDate))
                     episode.setDescription(description)
                     episode.setItunesImage(itunesImage)
+                    episode.setItunesDuration(itunesDuration)
                     episode
                     //new EpisodeDTO(echoId, title, link, description, itunesImage)
                 }
