3b11a859017c79ef7c350f161759e8a66f82c26f
Maximilian Irro <max@irro.at>
2018-01-26 00:41:43 +0100

    [actor] use Cameo Pattern for handling requests from searcher to indexstore. I will use this pattern for all other synchronouse requests too in the future

---
+++ src/echo-actors/src/main/scala/echo/actor/searcher/IndexStoreReponseHandler.scala
@@ -0,0 +1,66 @@
+package echo.actor.searcher
+
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.event.LoggingReceive
+import echo.actor.protocol.ActorMessages.{IndexResultsFound, NoIndexResultsFound, SearchResults}
+import echo.actor.searcher.IndexStoreReponseHandler.IndexRetrievalTimeout
+import echo.core.dto.ResultWrapperDTO
+import org.jsoup.Jsoup
+import org.jsoup.safety.Whitelist
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+/**
+  * @author Maximilian Irro
+  */
+
+object IndexStoreReponseHandler {
+
+    case object IndexRetrievalTimeout
+
+    def props(indexStore: ActorRef, originalSender: Option[ActorRef]): Props = {
+        Props(new IndexStoreReponseHandler(indexStore, originalSender.get))
+    }
+}
+
+class IndexStoreReponseHandler(indexStore: ActorRef, originalSender: ActorRef) extends Actor with ActorLogging {
+    def receive = LoggingReceive {
+        case IndexResultsFound(query: String, results: ResultWrapperDTO) => {
+            log.info("Received " + results.getTotalHits + " results from index for query '" + query + "'")
+            timeoutMessager.cancel
+
+            // TODO remove <img> tags from description, I suspect it to cause troubles
+            // TODO this should probably better done while indexing! (so only has to be done one time instead of every retrieval
+            results.getResults.foreach( d => {
+                Option(d.getDescription).map(value => {
+                    val soupDoc = Jsoup.parse(value)
+                    soupDoc.select("img").remove
+                    // if not removed, the cleaner will drop the <div> but leave the inner text
+                    val clean = Jsoup.clean(soupDoc.body.html, Whitelist.basic)
+                    d.setDescription(clean)
+                })
+            })
+
+            sendResponseAndShutdown(SearchResults(results))
+        }
+        case NoIndexResultsFound(query: String) => {
+            log.info("Received NO results from index for query '" + query + "'")
+            sendResponseAndShutdown(SearchResults(new ResultWrapperDTO))
+        }
+        case IndexRetrievalTimeout => sendResponseAndShutdown(IndexRetrievalTimeout)
+        case _ =>
+    }
+
+    def sendResponseAndShutdown(response: Any) = {
+        originalSender ! response
+        log.debug("Stopping context capturing actor")
+        context.stop(self)
+    }
+
+    import context.dispatcher
+    val timeoutMessager = context.system.scheduler.
+        scheduleOnce(5 seconds) {
+            self ! IndexRetrievalTimeout
+        }
+}
--- src/echo-actors/src/main/scala/echo/actor/searcher/SearcherActor.scala
+++ src/echo-actors/src/main/scala/echo/actor/searcher/SearcherActor.scala
@@ -1,24 +1,9 @@
 package echo.actor.searcher
 
-import java.util.concurrent.TimeoutException
+import akka.actor.{Actor, ActorLogging, ActorRef}
+import echo.actor.protocol.ActorMessages._
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props}
-import akka.event.{Logging, LoggingReceive}
-import akka.pattern.ask
-import akka.util.Timeout
-import echo.actor.protocol.ActorMessages.{IndexResultsFound, _}
-import echo.actor.searcher.SearcherActor.IndexRetrievalTimeout
-import echo.core.dto.{DTO, ResultWrapperDTO}
-import org.jsoup.Jsoup
-import org.jsoup.safety.Whitelist
-
-import scala.concurrent.{Await, Future}
-import scala.concurrent.duration._
 import scala.language.postfixOps
-
-object SearcherActor {
-    case object IndexRetrievalTimeout
-}
 
 class SearcherActor (val indexStore : ActorRef) extends Actor with ActorLogging {
 
@@ -29,49 +14,9 @@ class SearcherActor (val indexStore : ActorRef) extends Actor with ActorLogging 
         case SearchRequest(query,page,size) => {
             log.info("Received SearchRequest('{}',{},{})", query, page, size)
 
-            val originalSender = sender
-            context.actorOf(Props(new Actor() {
-                def receive = LoggingReceive {
-                    case IndexResultsFound(query: String, results: ResultWrapperDTO) => {
-                        log.info("Received " + results.getTotalHits + " results from index for query '" + query + "'")
-
-                        // TODO remove <img> tags from description, I suspect it to cause troubles
-                        // TODO this should probably better done while indexing! (so only has to be done one time instead of every retrieval
-                        results.getResults.foreach( d => {
-                            Option(d.getDescription).map(value => {
-                                val soupDoc = Jsoup.parse(value)
-                                soupDoc.select("img").remove
-                                // if not removed, the cleaner will drop the <div> but leave the inner text
-                                val clean = Jsoup.clean(soupDoc.body.html, Whitelist.basic)
-                                d.setDescription(clean)
-                            })
-                        })
-
-                        sendResponseAndShutdown(SearchResults(results))
-                    }
-                    case NoIndexResultsFound(query: String) => {
-                        log.info("Received NO results from index for query '" + query + "'")
-                        sendResponseAndShutdown(SearchResults(new ResultWrapperDTO))
-                    }
-                    case IndexRetrievalTimeout => sendResponseAndShutdown(IndexRetrievalTimeout)
-                }
-
-                def sendResponseAndShutdown(response: Any) = {
-                    originalSender ! response
-                    log.debug("Stopping context capturing actor")
-                    context.stop(self)
-                }
-
-                log.info("Sending SearchIndex('{}',{},{}) message", query, page, size)
-
-                indexStore ! SearchIndex(query, page, size)
-
-                import context.dispatcher
-                val timeoutMessager = context.system.scheduler.
-                    scheduleOnce(5 seconds) {
-                        self ! IndexRetrievalTimeout
-                    }
-            }))
+            val originalSender = Some(sender) // this is important to not expose the handler
+            val handler = context.actorOf(IndexStoreReponseHandler.props(indexStore, originalSender), "searcher-cameo-message-handler")
+            indexStore.tell(SearchIndex(query, page, size), handler)
         }
 
     }
