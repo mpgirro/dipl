8616d69e27e27b87263be2e625c962519503989a
Maximilian Irro <max@irro.at>
2018-01-30 22:14:29 +0100

    [actor] removed all of the old in-memory mock database clutter

--- src/echo-actors/src/main/scala/echo/actor/store/DirectoryStore.scala
+++ src/echo-actors/src/main/scala/echo/actor/store/DirectoryStore.scala
@@ -21,64 +21,15 @@ import scala.collection.JavaConverters._
   */
 class DirectoryStore extends Actor with ActorLogging {
 
-    // podcastId -> (timestamp, status, [episodeIds], PodcastDTO)
-    private val podcastDB = scala.collection.mutable.Map.empty[String, (LocalDateTime,FeedStatus,scala.collection.mutable.Set[String], PodcastDTO)]
-
-    // episodeId -> (EpisodeDTO)
-    private val episodeDB = scala.collection.mutable.Map.empty[String, EpisodeDTO]
-
-    // TODO for now, podcasts only have one single feed
-    // podcastId -> FeedURLs
-    private val feedDB = scala.collection.mutable.Map.empty[String, String]
-
     private var crawler: ActorRef = _
     private var indexStore: ActorRef = _
 
     private var mockEchoIdGenerator = 0 // TODO replace with real ID gen
 
-    /*
-    @Autowired
-    var podcastRepository: PodcastRepository = null
-    */
-
-
-    /*
-    val springAppContext = new ClassPathXmlApplicationContext("application-context.xml")
-    val podcastRepository: PodcastRepository = springAppContext.getBean("podcastRepository").asInstanceOf[PodcastRepository]
-    */
-
-    /*
-    val factory: RepositoryFactorySupport = … // Instantiate factory here
-    val podcastRepository: PodcastRepository = factory.getRepository(classOf[PodcastRepository]);
-    */
-
-    /*
-    import org.springframework.data.jpa.repository.support.JpaRepositoryFactory
-    import org.springframework.data.repository.core.support.RepositoryProxyPostProcessor
-    import org.springframework.orm.jpa.JpaTransactionManager
-    import org.springframework.transaction.annotation.AnnotationTransactionAttributeSource
-    import org.springframework.transaction.interceptor.TransactionInterceptor
-
-    val emf = Persistence.createEntityManagerFactory("echo.core.model.domain")
-    val em = emf.createEntityManager
-
-    val xactManager = new JpaTransactionManager(emf)
-    val factory = new JpaRepositoryFactory(emf.createEntityManager)
-
-    factory.addRepositoryProxyPostProcessor(new RepositoryProxyPostProcessor() {
-        def postProcess(factory: ProxyFactory): Unit = {
-            factory.addAdvice(new TransactionInterceptor(xactManager, new AnnotationTransactionAttributeSource))
-
-
-        }
-    })
-    */
     val appCtx = new ClassPathXmlApplicationContext("application-context.xml")
     val podcastDao: PodcastDao = appCtx.getBean(classOf[PodcastDao])
     val episodeDao: EpisodeDao = appCtx.getBean(classOf[EpisodeDao])
     val feedDao: FeedDao = appCtx.getBean(classOf[FeedDao])
-
-
 
 
     /*
@@ -112,9 +63,6 @@ class DirectoryStore extends Actor with ActorLogging {
     // Create the repository proxy instance
     val podcastRepository: PodcastRepository = repositoryFactory.getRepository(classOf[PodcastRepository])
 
-
-    //val podcastRepository: PodcastRepository = appCtx.getBean(classOf[PodcastRepository])
-
     override def receive: Receive = {
 
         case ActorRefCrawlerActor(ref) => {
@@ -129,9 +77,6 @@ class DirectoryStore extends Actor with ActorLogging {
         case ProposeNewFeed(feedUrl) => {
             log.debug("Received msg proposing a new feed: " + feedUrl)
 
-            //val fakePodcastId = "pfake" + { mockEchoIdGenerator += 1; mockEchoIdGenerator }
-
-
             /*
             Url62.create
             // 7NLCAyd6sKR7kDHxgAWFPG
@@ -140,29 +85,6 @@ class DirectoryStore extends Actor with ActorLogging {
             // c3587ec5-0976-497f-8374-61e0c2ea3da5
 
             Url62.encode(UUID.fromString("c3587ec5-0976-497f-8374-61e0c2ea3da5"))
-            */
-
-            /*
-            if(podcastDB.contains(fakePodcastId)){
-                // TODO remove the auto update
-                log.debug("Feed already in directory; will send an update request to crawler: {}", feedUrl)
-                val (_,_,episodes,_) = podcastDB(fakePodcastId)
-                crawler ! FetchUpdateFeed(feedUrl, fakePodcastId, episodes.toArray)
-            } else {
-                log.debug("Feed not yet known; will be passed to crawler: {}", feedUrl)
-
-                // create a new entry in PodcastDB
-                val podcastEntry = (LocalDateTime.now(), FeedStatus.NEVER_CHECKED, scala.collection.mutable.Set[String](), null)
-                podcastDB += (fakePodcastId -> podcastEntry)
-
-                // create a new entry in FeedDB
-                val feedEntry = feedUrl
-                feedDB += (fakePodcastId -> feedEntry)
-
-                // do this at the end, to be sure that the database contains already the entries!
-                // TODO hier bin ich vielleicht zu abhängig davon das die werte schon in der datenbank stehen!
-                crawler ! FetchNewFeed(feedUrl, fakePodcastId)
-            }
             */
 
             // TODO check of feed is known yet
@@ -191,32 +113,8 @@ class DirectoryStore extends Actor with ActorLogging {
         }
 
         case FeedStatusUpdate(feedUrl, timestamp, status) => {
-
-            /*
-            var success = false;
-            for( (podcastId,f) <- feedDB){
-                if(f.equals(feedUrl)){
-                    log.debug("Received FeedStatusUpdate({},{},{})", feedUrl, timestamp, status)
-                    if(podcastDB.contains(podcastId)){
-                        val (_, _, episodes, podcast) = podcastDB(podcastId)
-                        val newEntry = (timestamp, status, episodes, podcast)
-
-                        // note: database.updated(...) does not work for some reason
-                        podcastDB.remove(podcastId)
-                        podcastDB += (podcastId -> newEntry)
-
-                        success = true
-                    } else {
-                        log.error(s"Found a Podcast id=$podcastId in FeedDB, but it could not be found in PodcastDB")
-                    }
-                }
-            }
-            if(!success){
-                log.error("Received UNKNOWN FEED/Podcast FeedStatusUpdate({},{},{})", feedUrl, timestamp, status)
-            }
-            */
-
             log.debug("Received FeedStatusUpdate({},{},{})", feedUrl, timestamp, status)
+
             feedDao.findByUrl(feedUrl).map(feed => {
                 feed.setLastChecked(DateMapper.INSTANCE.asTimestamp(timestamp))
                 feed.setLastStatus(status)
@@ -230,17 +128,6 @@ class DirectoryStore extends Actor with ActorLogging {
 
         case UpdatePodcastMetadata(docId: String, podcast: PodcastDTO) => {
             log.debug("Received UpdatePodcastMetadata({},{})", docId, podcast)
-
-            /*
-            // TODO for now, we only set the itunesImage in the fake database
-            if(podcastDB.contains(docId)){
-                val (timestamp, status, episodes, _) = podcastDB(docId)
-                val newEntry = (timestamp, status, episodes, podcast)
-                podcastDB += (docId -> newEntry)
-            } else {
-                log.error("Received a UpdatePodcastMetadata for an unknown podcast document claiming docId: {}", docId)
-            }
-            */
 
             /* * * * * * * * * * * * * * * * * * *
              * TODO this is the new database code
@@ -257,22 +144,6 @@ class DirectoryStore extends Actor with ActorLogging {
         }
 
         case UpdateEpisodeMetadata(podcastDocId: String, episode: EpisodeDTO) => {
-            // TODO
-
-            /*
-            if(podcastDB.contains(podcastDocId)){
-                log.debug("Received UpdateEpisodeMetadata({},{})", podcastDocId, episode)
-                val (timestamp, status, episodes, podcast) = podcastDB(podcastDocId)
-                episodes += episode.getEchoId
-                val newEntry = (timestamp, status, episodes, podcast)
-
-                podcastDB.remove(podcastDocId)
-                podcastDB += (podcastDocId -> newEntry)
-            } else {
-                log.error("Received a UpdateEpisodeMetadata for an unknown podcast document claiming docId: {}", podcastDocId)
-            }
-            episodeDB += (episode.getEchoId -> episode)
-            */
 
             /* * * * * * * * * * * * * * * * * * *
              * TODO this is the new database code
@@ -303,28 +174,6 @@ class DirectoryStore extends Actor with ActorLogging {
         case UsePodcastItunesImage(echoId) => {
             log.debug("Received UsePodcastItunesImage({})", echoId)
 
-            /*
-            var found = false
-            for((_,(_,_,episodes,podcast)) <- podcastDB){
-                if(episodes.contains(echoId)){
-
-                    // first ('cause concurrency 'n stuff) we send along the notification to update the image in the index
-                    indexStore ! IndexStoreUpdateEpisodeAddItunesImage(echoId,podcast.getItunesImage)
-
-                    // then we update the info in our own local database
-                    if(episodeDB.contains(echoId)){
-                        val episode = episodeDB(echoId)
-                        episode.setItunesImage(podcast.getItunesImage)
-                    } else {
-                        log.error("Episode echoId={} was found as a member of a podcast, but is not present in the Episode table")
-                    }
-                }
-            }
-            if(found){
-                log.info("Did not find Episode with echoId={} in the database (could not set its itunesImage therefore)", echoId)
-            }
-            */
-
             episodeDao.findByEchoId(echoId).map(e => {
                 val p = e.getPodcast
                 if(p != null){
@@ -342,15 +191,6 @@ class DirectoryStore extends Actor with ActorLogging {
         case GetPodcast(echoId) => {
             log.debug("Received GetPodcast('{}')", echoId)
 
-            /*
-            if(podcastDB.contains(echoId)){
-                sender ! PodcastResult(podcastDB(echoId)._4)
-            } else {
-                log.error("Database does not contain Podcast with echoId={}", echoId)
-                sender ! NoDocumentFound(echoId)
-            }
-            */
-
             podcastDao.findByEchoId(echoId).map(p => {
                 val podcast = PodcastMapper.INSTANCE.podcastToPodcastDto(p)
                 sender ! PodcastResult(podcast)
@@ -367,15 +207,6 @@ class DirectoryStore extends Actor with ActorLogging {
         case GetEpisode(echoId) => {
             log.debug("Received GetEpisode('{}')", echoId)
 
-            /*
-            if(episodeDB.contains(echoId)){
-                sender ! EpisodeResult(episodeDB(echoId))
-            } else {
-                log.error("Database does not contain Episode with echoId={}", echoId)
-                sender ! NoDocumentFound(echoId)
-            }
-            */
-
             episodeDao.findByEchoId(echoId).map(e => {
                 val episode = EpisodeMapper.INSTANCE.episodeToEpisodeDto(e)
                 sender ! EpisodeResult(episode)
@@ -387,27 +218,6 @@ class DirectoryStore extends Actor with ActorLogging {
 
         case GetEpisodesByPodcast(podcastId) => {
             log.debug("Received GetEpisodesByPodcast('{}')", podcastId)
-
-            /*
-            if(podcastDB.contains(podcastId)){
-
-                val (_,_,episodes,_) = podcastDB(podcastId)
-                var results = scala.collection.mutable.ArrayBuffer.empty[EpisodeDTO]
-
-                for(episodeId <- episodes){
-                    if(episodeDB.contains(episodeId)){
-                        results += episodeDB(episodeId)
-                    } else {
-                        log.error(s"Podcast Table entry for podcast=$podcastId references an Episode with echoId=$episodeId , but Episode Table has no entry for this id")
-                    }
-                }
-
-                sender ! EpisodesByPodcastResult(results.toArray)
-            } else {
-                log.error("Database does not contain Podcast with echoId={}", podcastId)
-                sender ! NoDocumentFound(podcastId)
-            }
-            */
 
             podcastDao.findByEchoId(podcastId).map(p => {
                 val episodes = episodeDao.getAllByPodcast(p).asJava
@@ -422,11 +232,7 @@ class DirectoryStore extends Actor with ActorLogging {
 
         case DebugPrintAllDatabase => {
             log.debug("Received DebugPrintAllDatabase")
-            /*
-            for( (k,v) <- podcastDB){
-                println("docId: "+k+"\t"+v)
-            }
-            */
+
             println("------------------------")
             println("All Podcasts in database")
             podcastDao.getAll.map(p => println(p.getTitle))
@@ -439,16 +245,6 @@ class DirectoryStore extends Actor with ActorLogging {
     //@Transactional(readOnly=true) // TODO causes this compile errors?
     def getAllPodcasts(sender: ActorRef): Unit = {
         log.debug("Received GetAllPodcasts()")
-
-        /*
-        var results = scala.collection.mutable.ArrayBuffer.empty[PodcastDTO]
-        for((_,_,_,podcast: PodcastDTO) <- podcastDB.values){
-            results += podcast
-        }
-
-        sender ! AllPodcastsResult(results.toArray)
-        */
-
 
         val podcasts = podcastRepository.findAll()
         val podcastDTOs = PodcastMapper.INSTANCE.podcastsToPodcastDtos(podcasts)
