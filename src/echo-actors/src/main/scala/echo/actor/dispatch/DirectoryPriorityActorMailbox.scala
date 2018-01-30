acabb8711299d7ae4da100d1ae0f6a338e04693c
Maximilian Irro <max@irro.at>
2018-01-30 22:08:32 +0100

    [actor] forgot to commit the priority mailbox…

---
+++ src/echo-actors/src/main/scala/echo/actor/dispatch/DirectoryPriorityActorMailbox.scala
@@ -0,0 +1,24 @@
+package echo.actor.dispatch
+
+import akka.actor.ActorSystem
+import akka.dispatch.{PriorityGenerator, UnboundedPriorityMailbox}
+import com.typesafe.config.Config
+import echo.actor.protocol.ActorMessages._
+
+/**
+  * @author Maximilian Irro
+  */
+class DirectoryPriorityActorMailbox(settings: ActorSystem.Settings, config: Config) extends UnboundedPriorityMailbox(
+    // Create a new PriorityGenerator, lower prio means more important
+    PriorityGenerator {
+        case ActorRefCrawlerActor(_)    => 0
+        case ActorRefIndexStoreActor(_) => 0
+        case GetPodcast(_)              => 1
+        case GetAllPodcasts()           => 1
+        case GetEpisode(_)              => 1
+        case GetEpisodesByPodcast(_)    => 1
+        case DebugPrintAllDatabase()    => 1
+        case UpdatePodcastMetadata(_,_) => 2
+        case UpdateEpisodeMetadata(_,_) => 2
+        case _                          => 3
+    })
