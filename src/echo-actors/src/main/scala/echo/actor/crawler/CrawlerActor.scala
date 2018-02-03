f303f597006089ba8fc9531af2291dab8af96aac
Maximilian Irro <max@irro.at>
2018-02-03 17:26:44 +0100

    [actor] downloading content now with apache http client. this is way faster now

--- src/echo-actors/src/main/scala/echo/actor/crawler/CrawlerActor.scala
+++ src/echo-actors/src/main/scala/echo/actor/crawler/CrawlerActor.scala
@@ -1,6 +1,6 @@
 package echo.actor.crawler
 
-import java.io.IOException
+import java.io.{IOException, InputStreamReader}
 import java.net.{HttpURLConnection, SocketTimeoutException, URL}
 import java.time.LocalDateTime
 import java.util.Scanner
@@ -9,6 +9,12 @@ import akka.actor.{Actor, ActorLogging, ActorRef}
 import echo.actor.ActorProtocol._
 import echo.core.model.feed.FeedStatus
 import echo.core.parse.api.FyydAPI
+import org.apache.commons.io.IOUtils
+import org.apache.http.{HttpResponse, HttpStatus}
+import org.apache.http.client.HttpClient
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.impl.nio.client.{CloseableHttpAsyncClient, HttpAsyncClients}
 
 class CrawlerActor extends Actor with ActorLogging {
 
@@ -31,7 +37,7 @@ class CrawlerActor extends Actor with ActorLogging {
             log.info("Received FetchNewFeed('{}')", url)
 
             try {
-                val data = download(url)
+                val data = download_v2(url)
                 data match {
                     case Some(xml) =>
                         parser ! ParseFeedData(url, podcastId, xml)
@@ -50,13 +56,13 @@ class CrawlerActor extends Actor with ActorLogging {
 
             log.info("Received FetchUpdateFeed('{}')", url)
             try {
-                val data = download(url)
+                val data = download_v2(url)
                 data match {
                     case Some(xml) =>
                         parser ! ParseFeedData(url, podcastId, xml)
                         directoryStore ! FeedStatusUpdate(url, LocalDateTime.now(), FeedStatus.DOWNLOAD_SUCCESS)
                     case None =>
-                        log.error("Received NULL trying to download feed (update)from URL: {}", url)
+                        //log.error("Received NULL trying to download feed (update)from URL: {}", url)
                         directoryStore ! FeedStatusUpdate(url, LocalDateTime.now(), FeedStatus.DOWNLOAD_ERROR)
                 }
             } catch {
@@ -69,10 +75,11 @@ class CrawlerActor extends Actor with ActorLogging {
             log.debug("Received FetchWebsite({},'{}')", echoId, url)
 
             try{
-                val data = download(url)
+                val data = download_v2(url)
                 data match {
                     case Some(html) => parser ! ParseWebsiteData(echoId, html)
-                    case None       => log.error("Received NULL trying to download website data for URL: {}", url)
+                    case None       => // we simply have no website data to add to the index --> ignore and move on
+                        //log.error("Received NULL trying to download website data for URL: {}", url)
                 }
             } catch {
                 case e: IOException =>
@@ -82,7 +89,7 @@ class CrawlerActor extends Actor with ActorLogging {
 
         case CrawlFyyd(count) =>
             log.debug("Received CrawlFyyd({})", count)
-            val feeds = fyydAPI.getFeedUrls(count);
+            val feeds = fyydAPI.getFeedUrls(count)
             log.debug("Received {} feeds from {}", feeds.size, fyydAPI.getURL)
 
             log.debug("Proposing these feeds to the internal directory now")
@@ -195,14 +202,75 @@ class CrawlerActor extends Actor with ActorLogging {
                 return Some(scanner.next)
             }
         } catch {
-            case e: SocketTimeoutException => {
+            case e: SocketTimeoutException =>
                 log.error("Timout while loading src from URL: {} [reason: {}]", url, e.getMessage)
-            }
-            case e: java.lang.StackOverflowError => {
+            case e: java.lang.StackOverflowError =>
                 log.error("StackOverflowError trying to download: {} [reason: {}]", url, e.getMessage)
-            }
         }
         None
+    }
+
+    private def download_v2(url: String): Option[String] ={
+
+        try{
+            val client: HttpClient = HttpClientBuilder.create().build()
+
+            val response: HttpResponse = client.execute(new HttpGet(url))
+
+            val statusCode: Int = response.getStatusLine.getStatusCode
+            statusCode match {
+                case 200 => // all fine
+                case 404 => // not found: nothing there worth processing
+                    log.warning("404 behind : {}", url)
+                    return None
+                case _   => log.warning("Got unexpected status={} from downloading url={}", statusCode, url)
+            }
+
+            val contentType = response.getEntity.getContentType
+            val mimeType = contentType.getValue.split(";")(0).trim
+            mimeType match {
+                case "application/rss+xml" => // feed
+                case "application/xml"     => // feed
+                case "text/xml"            => // feed
+                case "text/html"           => // website
+                case "audio/mpeg"          =>
+                    log.error("Refused to download 'audio/mpeg' from url={}", url)
+                    return None
+                case _ => log.warning("Got unexpected MIME type='{}' of response from url={}", mimeType, url)
+            }
+
+            val in = response.getEntity.getContent
+            val html: String = IOUtils.toString(new InputStreamReader(in))
+
+            Option(html)
+        } catch {
+            case e: java.net.UnknownHostException =>
+                log.error("UnknownHostException for url={}", url)
+                // TODO I should rethrow this exception, and then send a message that to mark the feed as invalid (dont crawl it again?)
+                None
+            case e: Exception =>
+                e.printStackTrace()
+                None
+        }
+    }
+
+    // TODO do something useful with the future, e.g start a response handler child actor
+    private def downloadAsync(url: String): Option[String] ={
+        val httpclient: CloseableHttpAsyncClient = HttpAsyncClients.createDefault()
+        // Start the client
+        httpclient.start()
+
+        // Execute request
+        val request: HttpGet = new HttpGet(url)
+        val future: java.util.concurrent.Future[HttpResponse] = httpclient.execute(request, null);
+
+        // and wait until a response is received
+        val response: HttpResponse = future.get()
+
+        val in = response.getEntity.getContent
+        val html: String = IOUtils.toString(new InputStreamReader(in))
+
+        Option(html)
     }
 
 }
