877672d708a893fd1f9352f36297b9ccf4e4a6d1
Maximilian Irro <max@irro.at>
2018-02-04 16:16:47 +0100

    [actor] adding website data and itunes image requests are now queued in indexStore and batch-processed to save commiting; commits happen only every 10 seconds. seems to greatly improve performance with growing index size

--- src/echo-actors/src/main/scala/echo/actor/index/IndexStore.scala
+++ src/echo-actors/src/main/scala/echo/actor/index/IndexStore.scala
@@ -5,9 +5,10 @@ import com.typesafe.config.ConfigFactory
 import echo.actor.ActorProtocol._
 import echo.core.exception.SearchException
 import echo.core.index.{IndexCommitter, LuceneCommitter}
-import echo.core.model.dto.EpisodeDTO
+import echo.core.model.dto.{DTO, EpisodeDTO}
 import echo.core.search.{IndexSearcher, LuceneSearcher}
 
+import scala.collection.mutable
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
@@ -15,12 +16,14 @@ class IndexStore extends Actor with ActorLogging {
 
     private val INDEX_PATH = ConfigFactory.load().getString("echo.index")
 
-    private val COMMIT_INTERVAL = 3000 millis // TODO read from config file
+    private val COMMIT_INTERVAL = 10 seconds // TODO read from config file
 
     private val indexCommitter: IndexCommitter = new LuceneCommitter(INDEX_PATH, true) // TODO do not alway re-create the index
     private val indexSearcher: IndexSearcher = new LuceneSearcher(indexCommitter.asInstanceOf[LuceneCommitter].getIndexWriter)
 
     private var indexChanged = false
+    private val websiteQueue: mutable.Queue[(String,String)] = new mutable.Queue
+    private val imageQueue: mutable.Queue[(String,String)] = new mutable.Queue
 
     import context.dispatcher
     private val commitMessager: Cancellable = context.system.scheduler.
@@ -30,10 +33,63 @@ class IndexStore extends Actor with ActorLogging {
 
     private def commitIndexIfChanged(): Unit = {
         if(indexChanged) {
-            log.debug("Index changed, committing now")
+            log.debug("Committing Index due to pending changes")
             indexCommitter.commit()
             indexChanged = false
         }
+
+        if(!websiteQueue.isEmpty){
+            log.debug("Processing pending entries in website queue")
+            indexSearcher.refresh()
+            processWebsiteQueue(websiteQueue)
+            indexCommitter.commit()
+        }
+
+        if(!imageQueue.isEmpty){
+            log.debug("Processing pending entries in image queue")
+            indexSearcher.refresh()
+            processImageQueue(imageQueue)
+            indexCommitter.commit()
+        }
+    }
+
+    private def processWebsiteQueue(queue: mutable.Queue[(String,String)]): Unit = {
+        if(queue.isEmpty) {
+            return
+        }
+
+        val (echoId,html) = queue.dequeue()
+        val entry = Option(indexSearcher.findByEchoId(echoId))
+        entry match {
+            case Some(doc) =>
+                doc.setWebsiteData(html)
+                indexCommitter.update(doc)
+            case None => log.error("Could not retrieve from index: echoId={}", echoId)
+        }
+
+        processWebsiteQueue(queue)
+    }
+
+    private def processImageQueue(queue: mutable.Queue[(String,String)]): Unit = {
+
+        if(queue.isEmpty) return
+
+        val (echoId,itunesImage) = queue.dequeue()
+        val entry = Option(indexSearcher.findByEchoId(echoId))
+        entry match {
+            case Some(doc) =>
+                doc match {
+                    case e: EpisodeDTO =>
+                        doc.setItunesImage(itunesImage)
+                        indexCommitter.update(doc)
+                        indexChanged = true
+                    case _ =>
+                        log.error("Retrieved a Document by ID from Index that is not an EpisodeDocument, though I expected one")
+                }
+            case None => log.error("Could not retrieve from index: echoId={}", echoId)
+        }
+
+        processImageQueue(queue)
     }
 
     override def receive: Receive = {
@@ -68,6 +124,10 @@ class IndexStore extends Actor with ActorLogging {
              * I could add a tryCount argument to the message (e.g. initial 3), and in case i cannot find a document
              * then resend the message to self with tryCount-1. if a message with tryCount=0 arrives, the the message is ignored and dropped
              */
+
+            websiteQueue.enqueue((echoId,html))
+
+            /*
             commitIndexIfChanged() // ensure that the Podcast/Episode document is committed to the document (the message should already be processed at this point
 
             indexSearcher.refresh()
@@ -79,6 +139,7 @@ class IndexStore extends Actor with ActorLogging {
                     indexChanged = true
                 case None => log.error("Could not retrieve from index: echoId={}", echoId)
             }
+            */
 
         case IndexStoreUpdateDocItunesImage(echoId, itunesImage) =>
             log.debug("Received IndexStoreUpdateDocItunesImage({},{})", echoId, itunesImage)
@@ -87,6 +148,10 @@ class IndexStore extends Actor with ActorLogging {
              * I could add a tryCount argument to the message (e.g. initial 3), and in case i cannot find a document
              * then resend the message to self with tryCount-1. if a message with tryCount=0 arrives, the the message is ignored and dropped
              */
+
+            imageQueue.enqueue((echoId, itunesImage))
+
+            /*
             commitIndexIfChanged() // ensure that the Podcast/Episode document is committed to the document (the message should already be processed at this point
 
             indexSearcher.refresh()
@@ -104,6 +169,7 @@ class IndexStore extends Actor with ActorLogging {
                     }
                 case None => log.error("Could not retrieve from index: echoId={}", echoId)
             }
+            */
 
         case SearchIndex(query, page, size) =>
             log.info("Received SearchIndex('{}',{},{}) message", query, page, size)
