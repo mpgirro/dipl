e6208e9eea7addb7b3696ffc4f3a39c26b906037
Maximilian Irro <max@irro.at>
2018-02-20 19:36:07 +0100

    [core] merged all teaser mappers into one

--- src/echo-core/src/main/java/echo/core/mapper/TeaserMapper.java
+++ src/echo-core/src/main/java/echo/core/mapper/TeaserMapper.java
@@ -1,7 +1,9 @@
 package echo.core.mapper;
 
 import echo.core.model.domain.Episode;
+import echo.core.model.domain.Podcast;
 import echo.core.model.dto.EpisodeDTO;
+import echo.core.model.dto.PodcastDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
@@ -11,10 +13,20 @@ import org.mapstruct.factory.Mappers;
  * @author Maximilian Irro
  */
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        uses={PodcastMapper.class, DateMapper.class})
-public interface EpisodeTeaserMapper {
+    uses={UrlMapper.class, DateMapper.class})
+public interface TeaserMapper {
 
-    EpisodeTeaserMapper INSTANCE = Mappers.getMapper( EpisodeTeaserMapper.class );
+    TeaserMapper INSTANCE = Mappers.getMapper( TeaserMapper.class );
+
+    @Mapping(target = "echoId")
+    @Mapping(target = "title")
+    @Mapping(target = "itunesImage")
+    PodcastDTO asTeaser(PodcastDTO dto);
+
+    @Mapping(target = "echoId")
+    @Mapping(target = "title")
+    @Mapping(target = "itunesImage")
+    PodcastDTO asTeaser(Podcast podcast);
 
     @Mapping(target = "echoId")
     @Mapping(target = "title")
