dbaf710bf175308912c0b2c5baccbfb4bba259e6
Maximilian Irro <max@irro.at>
2018-03-02 23:50:11 +0100

    [msa] added skeleton for the directory microservice app

---
+++ src/echo-microservices/app-directory/src/main/java/echo/microservice/directory/DirectoryApp.java
@@ -0,0 +1,13 @@
+package echo.microservice.directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectoryApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DirectoryApp.class, args);
+  }
+
+}
