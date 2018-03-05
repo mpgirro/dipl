2322d8ffa39ef07213385dbef536d917bf557ad7
Maximilian Irro <max@irro.at>
2018-03-05 09:48:07 +0100

    [core] cleanup id generator

--- src/echo-core/src/main/java/echo/core/util/EchoIdGenerator.java
+++ src/echo-core/src/main/java/echo/core/util/EchoIdGenerator.java
@@ -1,59 +1,30 @@
 package echo.core.util;
 
 import org.hashids.Hashids;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.BitSet;
-import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Maximilian Irro
  */
 public class EchoIdGenerator {
 
+    private static final Logger log = LoggerFactory.getLogger(EchoIdGenerator.class);
+
     private final Hashids hashids = new Hashids("Bit useless for my purpose, but why not");
-    private final Long shardId;
-    private int seq;
+    private final Integer shardId;
+    private Integer seq;
 
-    public EchoIdGenerator(long shardId) {
-        this.shardId = shardId;  // should be 13 bit
-        this.seq = 0;            // should be 10bit
+    public EchoIdGenerator(int shardId) {
+        this.shardId = shardId;
+        this.seq = 0;
     }
 
-    public String getNewId(){
+    public String getNewId() {
         final long now = System.currentTimeMillis();
+        final String id = hashids.encode(now, shardId, seq);
         seq = (seq+1) % 1024;
-        return hashids.encode(now, shardId, seq);
-    }
-
-    @Deprecated
-    private int randomWithRange(int min, int max) {
-        final int range = (max - min) + 1;
-        return (int)(Math.random() * range) + min;
-    }
-
-    @Deprecated
-    private BitSet convert(long value) {
-        final BitSet bits = new BitSet();
-        int index = 0;
-        while (value != 0L) {
-            if (value % 2L != 0) {
-                bits.set(index);
-            }
-            ++index;
-            value = value >>> 1;
-        }
-        return bits;
-    }
-
-    @Deprecated
-    private long convert(BitSet bits) {
-        long value = 0L;
-        for (int i = 0; i < bits.length(); ++i) {
-            value += bits.get(i) ? (1L << i) : 0L;
-        }
-        return value;
+        return id;
     }
 
 }
