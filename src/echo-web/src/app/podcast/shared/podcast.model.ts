14cea088e1121d0c50849faff84c51d7dd6be24f
Maximilian Irro <max@irro.at>
2018-03-06 17:21:55 +0100

    [web] more fields for podcast model

--- src/echo-web/src/app/podcast/shared/podcast.model.ts
+++ src/echo-web/src/app/podcast/shared/podcast.model.ts
@@ -5,10 +5,18 @@ export class Podcast {
   link: string;
   description: string;
   pubDate: string;
-  language: string;
   image: string;
   itunesCategories: string[];
   itunesSummary: string;
   itunesAuthor: string;
   itunesKeywords: string;
+  itunesExplicit: boolean;
+  itunesBlock: boolean;
+  itunesType: string;
+  language: string;
+  generator: string;
+  copyright: string;
+  episodeCount: number;
+  registrationTimestamp: string;
+  registrationComplete: boolean;
 }
