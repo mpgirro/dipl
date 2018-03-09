86795a147d313dec62826861e2b75b3236e79965
Maximilian Irro <max@irro.at>
2018-03-09 13:47:28 +0100

    [msa] some cleanup in directory MS

--- src/echo-microservices/app-directory/src/main/java/echo/microservice/directory/config/DatabaseConfig.java
+++ src/echo-microservices/app-directory/src/main/java/echo/microservice/directory/config/DatabaseConfig.java
@@ -1,24 +1,14 @@
 package echo.microservice.directory.config;
 
-import liquibase.integration.spring.SpringLiquibase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 /**
  * @author Maximilian Irro
