8cc96447638fd5734daa7aec2989b9dac645ea08
Maximilian Irro <max@irro.at>
2018-03-16 22:55:18 +0100

    [msa] added async configurations to MS

---
+++ src/echo-microservices/app-index/src/main/java/echo/microservice/index/config/AsyncConfig.java
@@ -0,0 +1,54 @@
+package echo.microservice.index.config;
+
+import echo.microservice.index.async.ExceptionHandlingAsyncTaskExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @author Maximilian Irro
+ */
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class AsyncConfig implements AsyncConfigurer {
+
+    private final Logger log = LoggerFactory.getLogger(AsyncConfig.class);
+
+    @Value("${echo.index.core-pool-size:2}")
+    private Integer CORE_POOL_SIZE;
+
+    @Value("${echo.index.max-pool-size:50}")
+    private Integer MAX_POOL_SIZE;
+
+    @Value("${echo.index.queue-capacity:10000}")
+    private Integer QUEUE_CAPACITY;
+
+    @Override
+    @Bean(name = "taskExecutor")
+    public Executor getAsyncExecutor() {
+        log.debug("Creating Async Task Executor");
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setThreadNamePrefix("echo-index-Executor-");
+        return new ExceptionHandlingAsyncTaskExecutor(executor);
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
+    }
+
+}
