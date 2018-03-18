b4c181f5a276d41688963398a2ee0d909e50acc5
Maximilian Irro <max@irro.at>
2018-03-18 14:53:08 +0100

    [msa] added configuration to marshall dates to JSON

--- src/echo-microservices/app-crawler/src/main/java/echo/microservice/crawler/config/JacksonConfig.java
+++ src/echo-microservices/app-crawler/src/main/java/echo/microservice/crawler/config/JacksonConfig.java
@@ -1,17 +1,40 @@
 package echo.microservice.crawler.config;
 
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
+import echo.microservice.crawler.util.JSR310DateTimeSerializer;
+import echo.microservice.crawler.util.JSR310ZonedDateTimeDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.zalando.problem.ProblemModule;
 import org.zalando.problem.validation.ConstraintViolationProblemModule;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
 
 /**
  * @author Maximilian Irro
  */
 @Configuration
 public class JacksonConfig {
+
+    @Bean
+    Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        JavaTimeModule module = new JavaTimeModule();
+        module.addSerializer(OffsetDateTime.class, JSR310DateTimeSerializer.INSTANCE);
+        module.addSerializer(ZonedDateTime.class, JSR310DateTimeSerializer.INSTANCE);
+        module.addSerializer(ZonedDateTime.class, JSR310DateTimeSerializer.INSTANCE);
+        module.addSerializer(Instant.class, JSR310DateTimeSerializer.INSTANCE);
+        module.addDeserializer(ZonedDateTime.class, JSR310ZonedDateTimeDeserializer.INSTANCE);
+        return new Jackson2ObjectMapperBuilder()
+            .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            .findModulesViaServiceLoader(true)
+            .modulesToInstall(module);
+    }
 
     /*
      * Support for Hibernate types in Jackson.
