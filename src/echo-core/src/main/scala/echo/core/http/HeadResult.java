d0bc09a6ab9c81313461d88d5403ac0e896f13c6
Maximilian Irro <max@irro.at>
2018-03-21 10:01:18 +0100

    [core] http client now evaluates the character encoding of a resource from a head request, and has option to set the to be used encoding for the fetch

--- src/echo-core/src/main/scala/echo/core/http/HeadResult.java
+++ src/echo-core/src/main/scala/echo/core/http/HeadResult.java
@@ -1,56 +1,57 @@
 package echo.core.http;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+import java.beans.ConstructorProperties;
 import java.util.Optional;
 
 /**
+ * This is an **immutable** DTO, with Jackson serialization capability (see https://blog.pchudzik.com/201704/immutable-dto/)
+ *
  * @author Maximilian Irro
  */
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class HeadResult {
 
-    private Integer statusCode;
-    private Optional<String> location;
-    private Optional<String> mimeType;
-    private Optional<String> eTag;
-    private Optional<String> lastModified;
+    private final Integer statusCode;
+    private final Optional<String> location;
+    private final Optional<String> mimeType;
+    private final Optional<String> contentEncoding;
+    private final Optional<String> eTag;
+    private final Optional<String> lastModified;
+
+    @ConstructorProperties({"statusCode", "location", "mimeType", "contentEncoding", "eTag", "lastModified"})
+    public HeadResult(Integer statusCode, Optional<String> location, Optional<String> mimeType, Optional<String> contentEncoding, Optional<String> eTag, Optional<String> lastModified) {
+        this.statusCode = statusCode;
+        this.location = location;
+        this.mimeType = mimeType;
+        this.contentEncoding = contentEncoding;
+        this.eTag = eTag;
+        this.lastModified = lastModified;
+    }
 
     public Integer getStatusCode() {
         return statusCode;
-    }
-
-    public void setStatusCode(Integer statusCode) {
-        this.statusCode = statusCode;
     }
 
     public Optional<String> getLocation() {
         return location;
     }
 
-    public void setLocation(Optional<String> location) {
-        this.location = location;
-    }
-
     public Optional<String> getMimeType() {
         return mimeType;
     }
 
-    public void setMimeType(Optional<String> mimeType) {
-        this.mimeType = mimeType;
+    public Optional<String> getContentEncoding() {
+        return contentEncoding;
     }
 
     public Optional<String> geteTag() {
         return eTag;
     }
 
-    public void seteTag(Optional<String> eTag) {
-        this.eTag = eTag;
-    }
-
     public Optional<String> getLastModified() {
         return lastModified;
-    }
-
-    public void setLastModified(Optional<String> lastModified) {
-        this.lastModified = lastModified;
     }
 
     @Override
@@ -59,6 +60,7 @@ public class HeadResult {
             "statusCode=" + statusCode +
             ", location=" + location +
             ", mimeType=" + mimeType +
+            ", contentEncoding=" + contentEncoding +
             ", eTag=" + eTag +
             ", lastModified=" + lastModified +
             '}';
--- src/echo-core/src/main/scala/echo/core/http/HttpClient.scala
+++ src/echo-core/src/main/scala/echo/core/http/HttpClient.scala
@@ -4,16 +4,21 @@ import com.softwaremill.sttp._
 import com.typesafe.scalalogging.Logger
 import echo.core.exception.EchoException
 
-import scala.concurrent.duration.{Duration, FiniteDuration}
 import scala.compat.java8.OptionConverters._
+import scala.concurrent.duration._
 
 /**
+  *
+  * @param timeout timeout in seconds
+  * @param DOWNLOAD_MAXBYTES
   * @author Maximilian Irro
   */
-class HttpClient (val DOWNLOAD_TIMEOUT: FiniteDuration,
+class HttpClient (val timeout: Long,
                   val DOWNLOAD_MAXBYTES: Long) {
 
     private val log = Logger(classOf[HttpClient])
+
+    private val DOWNLOAD_TIMEOUT = timeout.seconds
 
     private implicit val sttpBackend = HttpURLConnectionBackend(options = SttpBackendOptions.connectionTimeout(DOWNLOAD_TIMEOUT))
 
@@ -29,9 +34,9 @@ class HttpClient (val DOWNLOAD_TIMEOUT: FiniteDuration,
     def headCheck(url: String): HeadResult = {
 
         val response = emptyRequest // use empty request, because standard req uses header "Accept-Encoding: gzip" which can cause problems with HEAD requests
-            .response(ignore)
-            .readTimeout(DOWNLOAD_TIMEOUT)
             .head(uri"${url}")
+            .readTimeout(DOWNLOAD_TIMEOUT)
+            .acceptEncoding("")
             .send()
 
         // we assume we will use the known URL to download later, but maybe this changes...
@@ -41,7 +46,6 @@ class HttpClient (val DOWNLOAD_TIMEOUT: FiniteDuration,
             response.code match {
                 case 200 => // all fine
                 case 301 => // Moved Permanently
-                    // TODO do something with the new location, e.g. send message to directory to update episode, and use this to (re-)index the new website
                     location = response.header("location")
                     log.debug("Redirecting {} to {}", url, location.getOrElse("NON PROVIDED"))
                 case 302 => // odd, but ok
@@ -73,20 +77,32 @@ class HttpClient (val DOWNLOAD_TIMEOUT: FiniteDuration,
                 log.warn("Did not get a Content-Type from HEAD request : {}", url)
         }
 
+        // extract the character encoding of the resource if it is returned, to avoid encoding problems
+        val encoding: Option[String] = response.contentType
+            .map(ct => ct.split(";")
+                .lift(1)                      // -> charset="UTF-8"
+                .map(_.split("=")
+                    .lift(1)                  // -> "UTF-8"
+                    .map( _
+                        .replaceAll("\"", "") // remove quotation marks if any
+                        .trim ))              // remove whitespace
+                    .getOrElse(None))
+            .getOrElse(None)
+
         //set the etag if existent
         val eTag: Option[String] = response.header("etag")
 
         //set the "last modified" header field if existent
         val lastModified: Option[String] = response.header("last-modified")
 
-        val result: HeadResult = new HeadResult
-        result.setStatusCode(response.code)
-        result.setMimeType(mimeType.asJava)
-        result.setLocation(location.asJava)
-        result.seteTag(eTag.asJava)
-        result.setLastModified(lastModified.asJava)
-
-        result
+        new HeadResult(
+            response.code,
+            location.asJava,
+            mimeType.asJava,
+            encoding.asJava,
+            eTag.asJava,
+            lastModified.asJava
+        )
     }
 
     @throws(classOf[EchoException])
@@ -94,13 +110,16 @@ class HttpClient (val DOWNLOAD_TIMEOUT: FiniteDuration,
     @throws(classOf[java.net.SocketTimeoutException])
     @throws(classOf[java.net.UnknownHostException])
     @throws(classOf[javax.net.ssl.SSLHandshakeException])
-    def fetchContent(url: String): String = {
+    def fetchContent(url: String, encoding: java.util.Optional[String]): String = {
 
-        val response = sttp
+        val request = sttp
             .get(uri"${url}")
             .readTimeout(DOWNLOAD_TIMEOUT)
-            .response(asString)
-            .send()
+            .response(asByteArray) // prevent assuming UTF-8 encoding, because some feeds do not use it
+
+        encoding.asScala.foreach(e => request.acceptEncoding(e))
+
+        val response = request.send()
 
         if (!response.isSuccess) {
             //log.error("Download resulted in a non-success response code : {}", response.code)
@@ -127,9 +146,10 @@ class HttpClient (val DOWNLOAD_TIMEOUT: FiniteDuration,
             case Left(errorMessage) =>
                 //log.error("Error collecting download body, message : {}", errorMessage)
                 throw new EchoException(s"Error collecting download body, message : ${errorMessage}") // TODO make dedicated exception
-            case Right(deserializedBody) =>
+            case Right(data) =>
                 log.debug("Finished collecting content from GET response : {}", url)
-                deserializedBody // this will be returned
+
+                new String(data, encoding.asScala.getOrElse("utf-8"))
         }
     }
 
