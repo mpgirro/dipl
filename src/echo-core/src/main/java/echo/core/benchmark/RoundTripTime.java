7562e78b035ab601e89df25c92f74b07804f3685
Maximilian Irro <max@irro.at>
2018-07-18 11:48:26 +0200

    [core] remodel Benchmark into dedicated RoundTripTime (again), added a workflow enum

--- src/echo-core/src/main/java/echo/core/benchmark/RoundTripTime.java
+++ src/echo-core/src/main/java/echo/core/benchmark/RoundTripTime.java
@@ -1,17 +1,48 @@
 package echo.core.benchmark;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.common.collect.ImmutableList;
+import org.immutables.value.Value;
+
+import javax.annotation.Nullable;
 
 /**
  * @author Maximilian Irro
  */
-public class RoundTripTime {
+@Value.Immutable
+@Value.Style(
+    jdkOnly    = true,              // prevent usage of Guava collections
+    get        = {"is*", "get*"},   // Detect 'get' and 'is' prefixes in accessor methods
+    init       = "set*",
+    create     = "new",             // generates public no args constructor
+    build      = "create"           // rename 'build' method on builder to 'create'
+)
+@JsonSerialize(as = ImmutableRoundTripTime.class)
+@JsonDeserialize(as = ImmutableRoundTripTime.class)
+public interface RoundTripTime {
 
-    public static synchronized ImmutableList<Long> appendNow(ImmutableList<Long> rtts) {
-        return ImmutableList.<Long>builder()
-            .addAll(rtts)
-            .add(System.currentTimeMillis())
-            .build();
+    String getUri();
+
+    String getLocation();
+
+    @Nullable
+    Workflow getWorkflow();
+
+    ImmutableList<Long> getRtts();
+
+    default RoundTripTime bumpRTTs() {
+        return ImmutableRoundTripTime.builder()
+            .from(this)
+            .addRtts(System.currentTimeMillis())
+            .create();
+    }
+
+    static RoundTripTime empty() {
+        return ImmutableRoundTripTime.builder()
+            .setUri("")
+            .setLocation("")
+            .create();
     }
 
 }
---
+++ src/echo-core/src/main/java/echo/core/benchmark/Workflow.java
@@ -0,0 +1,10 @@
+package echo.core.benchmark;
+
+/**
+ * @author Maximilian Irro
+ */
+public enum Workflow {
+    PODCAST_INDEX,
+    EPISODE_INDEX,
+    RESULT_RETRIEVAL
+}
