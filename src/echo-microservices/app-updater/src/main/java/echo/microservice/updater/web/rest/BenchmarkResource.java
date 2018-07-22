48a7abcf3d78019569b6f879bd73fb0727e0adcd
Maximilian Irro <max@irro.at>
2018-07-22 13:54:24 +0200

    [msa] start/stop/get MPS via REST interface

---
+++ src/echo-microservices/app-updater/src/main/java/echo/microservice/updater/web/rest/BenchmarkResource.java
@@ -0,0 +1,53 @@
+package echo.microservice.updater.web.rest;
+
+import echo.core.benchmark.MessagesPerSecondCounter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.net.URISyntaxException;
+
+/**
+ * @author Maximilian Irro
+ */
+@RestController
+@RequestMapping("/benchmark")
+public class BenchmarkResource {
+
+    private final Logger log = LoggerFactory.getLogger(BenchmarkResource.class);
+
+    @Resource(name = "messagesPerSecondCounter")
+    private MessagesPerSecondCounter mpsCounter;
+
+    @RequestMapping(
+        value  = "/start-mps",
+        method = RequestMethod.POST,
+        params = { "mps" })
+    @ResponseStatus(HttpStatus.OK)
+    public void startMpsCounting(@RequestParam("mps") @SuppressWarnings("unused") Boolean mps) throws URISyntaxException {
+        log.debug("REST request to start MPS counting");
+        mpsCounter.startCounting();
+    }
+
+    @RequestMapping(
+        value  = "/stop-mps",
+        method = RequestMethod.POST,
+        params = { "mps" })
+    @ResponseStatus(HttpStatus.OK)
+    public void stopMpsCounting(@RequestParam("mps") @SuppressWarnings("unused") Boolean mps) throws URISyntaxException {
+        log.debug("REST request to stop MPS counting");
+        mpsCounter.stopCounting();
+    }
+
+    @RequestMapping(
+        value    = "/get-mps",
+        method   = RequestMethod.GET)
+    public Double getMpsValue() {
+        log.debug("REST request to get MPS");
+        mpsCounter.incrementCounter();
+        return mpsCounter.getMessagesPerSecond();
+    }
+
+}
