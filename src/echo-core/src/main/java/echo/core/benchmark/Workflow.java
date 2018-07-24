f45061f3bb12d3ca350152d0ebc80d81d6fbfb32
Maximilian Irro <max@irro.at>
2018-07-24 19:29:10 +0200

    [core] enum to string

--- src/echo-core/src/main/java/echo/core/benchmark/Workflow.java
+++ src/echo-core/src/main/java/echo/core/benchmark/Workflow.java
@@ -1,10 +1,35 @@
 package echo.core.benchmark;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * @author Maximilian Irro
  */
 public enum Workflow {
-    PODCAST_INDEX,
-    EPISODE_INDEX,
-    RESULT_RETRIEVAL
+    PODCAST_INDEX("PODCAST_INDEX"),
+    EPISODE_INDEX("EPISODE_INDEX"),
+    RESULT_RETRIEVAL("RESULT_RETRIEVAL");
+
+    private String name;
+
+    private static final Map<String,Workflow> MAP;
+
+    static {
+        final Map<String,Workflow> map = new ConcurrentHashMap<>();
+        for (Workflow instance : Workflow.values()) {
+            map.put(instance.getName(),instance);
+        }
+        MAP = Collections.unmodifiableMap(map);
+    }
+
+    private Workflow(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
 }
