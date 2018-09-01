036a94c27a90a2de265f3093a39254be94b3f28d
Maximilian Irro <max@irro.at>
2018-09-01 15:08:08 +0200

    [eval] added some final evaluation data (raw)

--- eval/index-overall-eval.R
+++ eval/index-overall-eval.R
@@ -1,43 +1,51 @@
-akka_index_overall_data <- read.table("../src/benchmark/akka-index-rtt-overall.csv", header=T, sep=";");
-msa_index_overall_data <- read.table("../src/benchmark/msa-index-rtt-overall.csv", header=T, sep=";");
+index_akka_data <- read.table("data/index-akka-rtt-overall.csv", header=T, sep=";");
+index_msa_data <- read.table("data/index-msa-rtt-overall.csv", header=T, sep=";");
 
-akka_index_overall_data$color <- rep("red",nrow(akka_index_overall_data));
-msa_index_overall_data$color <- rep("blue",nrow(msa_index_overall_data));
+# change MS to seconds
+index_akka_data$overallRT <- index_akka_data$overallRT / 1000
+index_msa_data$overallRT <- index_msa_data$overallRT / 1000
 
-akka_index_overall_data
+#
+# This code calculates the mean value for Y if there several datapoints for a given X
+# The graph is then different to 
+#
+index_akka_data$color <- rep("red",nrow(index_akka_data));
+index_msa_data$color <- rep("blue",nrow(index_msa_data));
 
-a <- aggregate(akka_index_overall_data$overallRT, list(input_size=akka_index_overall_data$input_size), mean)
-b <- aggregate(msa_index_overall_data$overallRT, list(input_size=msa_index_overall_data$input_size), mean)
+a <- aggregate(index_akka_data$overallRT, list(input_size=index_akka_data$input_size), mean)
+b <- aggregate(index_msa_data$overallRT, list(input_size=index_msa_data$input_size), mean)
 
 plot(b$input_size, b$x, 
      type="l", pch=18, col="blue", lty=2, cex=1.2, 
-     xlab = "Input Feeds", ylab = "Overall Processing Time [ms]",
-     xlim=c(0,500), ylim=c(0, 400000))
+     xlab = "Input Feeds", ylab = "Overall Processing Time [seconds]",
+     xlim=c(0,500), ylim=c(0, 400))
 lines(a$input_size, a$x, pch=19, col="red", type="l")			   
 legend("topleft", legend=c("Akka", "MSA"),
        col=c("red", "blue"), lty=1:2, cex=1)
 
 
 
-
-x1 <- akka_index_overall_data$input_size;
-y1 <- akka_index_overall_data$overallRT;
+#
+# Plot the data points and draw approximation line
+#
+x1 <- index_akka_data$input_size;
+y1 <- index_akka_data$overallRT;
 x1.lm <- lm(y1~x1);
 
-x2 <- msa_index_overall_data$input_size;
-y2 <- msa_index_overall_data$overallRT;
+x2 <- index_msa_data$input_size;
+y2 <- index_msa_data$overallRT;
 x2.lm <- lm(y2~x2);
 
 
-search_overall_data <- rbind( akka_index_overall_data, msa_index_overall_data);
+search_overall_data <- rbind( index_akka_data, index_msa_data);
 
 
 plot(search_overall_data$input_size,search_overall_data$overallRT, 
      col=search_overall_data$color, 
      pch = 4,
      main = "Overall Round Trip Time for Search",
-     xlab = "Input Feeds", ylab = "Overall Processing Time [ms]",
-     xlim=c(0,200), ylim=c(0, 200000));
+     xlab = "Input Feeds", ylab = "Overall Processing Time [seconds]",
+     xlim=c(0,500), ylim=c(0, 400));
 abline(x1.lm, col="red");
 abline(x2.lm, col="blue", lty=2);
 legend("topleft", legend=c("Akka", "MSA"),
@@ -49,8 +57,8 @@ pdf("eval-index-overall.pdf");
 
 plot(x2, y2, 
      type="l", pch=18, col="blue", lty=2, cex=1.2, 
-     xlab = "Input Feeds", ylab = "Overall Processing Time [ms]",
-     xlim=c(0,200), ylim=c(0, 200000))
+     xlab = "Input Feeds", ylab = "Overall Processing Time [seconds]",
+     xlim=c(0,500), ylim=c(0, 400))
 lines(x1, y1, pch=19, col="red", type="l")			   
 legend("topleft", legend=c("Akka", "MSA"),
        col=c("red", "blue"), lty=1:2, cex=1)
